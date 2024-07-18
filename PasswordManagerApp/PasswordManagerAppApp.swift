741705a9ab22fbbbbc7938c719fdca9cfdc4b9ac
Sourabhp295 <sourabhp295@gmail.com>
2024-07-18 22:01:52 +0530

    Initial Commit

---
+++ PasswordManagerApp/PasswordManagerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PasswordManagerAppApp.swift
+//  PasswordManagerApp
+//
+//  Created by Sourabh Pandey on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PasswordManagerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
